c278ba24c6777807df99424fee2f857f5a1e615d
Dmitriy Alexandrov <duglas2099@gmail.com>
2023-07-31 16:42:08 +0300

    add _throttle on filters

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* global _:readonly */
 import './data.js';
 import './ads.js';
 import {setAdsFormSubmit, setFilterTypeChange, setFilterPriceChange, setFilterRoomsChange, setFilterGuestChange, setFilterFeaturesChange} from './form.js';
@@ -5,16 +6,16 @@ import {renderAds} from './map.js';
 import {getData} from './api.js';
 import {showAlert} from './util.js';
 
-
+const RENDER_DELAY = 500;
 
 getData ((ads) => {
   renderAds(ads);
   () => showAlert('Не удалось получить данные', 'red');
-  setFilterTypeChange(() => renderAds(ads));
-  setFilterPriceChange(() => renderAds(ads));
-  setFilterRoomsChange(() => renderAds(ads));
-  setFilterGuestChange(() => renderAds(ads));
-  setFilterFeaturesChange(() => renderAds(ads));
+  setFilterTypeChange(_.throttle(() => renderAds(ads), RENDER_DELAY));
+  setFilterPriceChange(_.throttle(() => renderAds(ads), RENDER_DELAY));
+  setFilterRoomsChange(_.throttle(() => renderAds(ads),RENDER_DELAY));
+  setFilterGuestChange(_.throttle(() => renderAds(ads),RENDER_DELAY));
+  setFilterFeaturesChange(_.throttle(() => renderAds(ads), RENDER_DELAY));
 });
 
 setAdsFormSubmit();
